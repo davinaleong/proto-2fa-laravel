c0ebb2f6040366ec982d486274c45b3420b4cb83
Davina Leong <leong.shi.yun@gmail.com>
2023-06-27 21:03:42 +0800

    Added code to generator 2FA QR code

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/complete-registration', [App\Http\Controllers\Auth\RegisterController::class, 'completeRegistration'])->name('complete-registration');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
